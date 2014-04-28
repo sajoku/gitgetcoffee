3dd4123b65a9b061a5e9bc18a5c5bbdbb6347c21
sajoku <sajoku@me.com>
2014-04-28 10:18:21 +0200

    Set the app color to beautiful turquoise

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -12,15 +12,25 @@ class AppDelegate
     T7.change_language('en') #Set the default language to english
 
 
+
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @root_controller = UINavigationController.alloc.initWithRootViewController(main_controller)
     @window.rootViewController =  @root_controller
     @window.makeKeyAndVisible
 
+    set_app_color
+
     true
   end
+
+  private
 
   def main_controller
     @main_controller ||= MainController.alloc.init
   end
+
+  def set_app_color
+    UIApplication.sharedApplication.keyWindow.tintColor = UIColor.turquoiseColor
+  end
+
 end
