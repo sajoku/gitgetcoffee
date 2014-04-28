151e27eb8bda0e698d6f8c957e6e5784c353217a
sajoku <sajoku@me.com>
2014-04-28 10:21:31 +0200

    Create labels for the followers and following githubbers

--- app/views/main_view.rb
+++ app/views/main_view.rb
@@ -7,6 +7,7 @@ module MainView
     create_username_label
     create_user_avatar
     create_followers_label
+    create_non_followers_label
   end
 
   def add_user_button
@@ -25,6 +26,10 @@ module MainView
     @followers_label ||= UILabel.alloc.initWithFrame(CGRectZero)
   end
 
+  def non_followers_label
+    @non_followers_label ||= UILabel.alloc.initWithFrame(CGRectZero)
+  end
+
   private
 
   def create_followers_label
@@ -35,20 +40,32 @@ module MainView
 
   def create_username_label
     username_label.color = UIColor.turquoiseColor
-    username_label.center = CGPointMake(130, 240)
+    username_label.center = CGPointMake(App.bounds.size.width / 2, 170)
+    username_label.text = "Moss"
+    username_label.sizeToFit
     self.view.addSubview(username_label)
   end
 
   def create_user_avatar
-    user_avatar.frame = [[80.0, 85.0], [150.0, 150.0]]
+    user_avatar.frame = [[0, 0], [100.0, 100.0]]
+    user_avatar.center = CGPointMake(App.bounds.size.width / 2, 120)
     user_avatar.clipsToBounds = true
-    user_avatar.layer.setCornerRadius(75)
+    user_avatar.layer.setCornerRadius(50)
 
     self.view.addSubview(user_avatar)
+  end
+
+  def create_non_followers_label
+    non_followers_label.color = UIColor.turquoiseColor
+    non_followers_label.center = CGPointMake(50, 320)
+    non_followers_label.numberOfLines = 0
+    non_followers_label.lineBreakMode = NSLineBreakByWordWrapping
+    self.view.addSubview(non_followers_label)
   end
 
 
   def user_avatar_image
     @user_avatar_image ||= UIImage.imageNamed("Untitled.png")
   end
+
 end
