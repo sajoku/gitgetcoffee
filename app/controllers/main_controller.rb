d330d4d7c2a28674c9300acc155e9e94528cd26b
sajoku <sajoku@me.com>
2014-05-01 21:36:35 +0200

    Move followers not following into tableview

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < UIViewController
   include MainView
-  attr_accessor :user, :label, :followers, :following
+
+  attr_accessor :user, :label, :followers, :following, :repos, :followers_not_following
 
   def viewDidLoad
     super
@@ -10,10 +11,15 @@ class MainController < UIViewController
     self.title = T7.t("Github - Social")
     self.view.backgroundColor = UIColor.whiteColor
 
+    add_followers_not_following_table
+
     true
   end
 
   def viewWillAppear(animated)
+    @followers_not_following = []
+    followers_not_following_table.reloadData
+
     if @user
       set_user
       get_followers
@@ -22,8 +28,6 @@ class MainController < UIViewController
   end
 
   def add_user(sender)
-    puts 'pressed ADD_USER'
-
     controller = NewUserFormController.alloc.init
     controller.parent = self
     self.navigationController.pushViewController(controller, animated: true)
@@ -40,15 +44,13 @@ class MainController < UIViewController
     avatar_data = NSData.alloc.initWithContentsOfURL(NSURL.URLWithString(@user.avatar_url))
     image = UIImage.alloc.initWithData(avatar_data)
     user_avatar.image = image
+
   end
 
   def get_followers
-    puts 'GETTING FOLLOWERS'
-    puts @user.followers_url
     AFMotion::SessionClient.shared.get(@user.followers_url) do |result|
       if result.success?
         @followers = []
-
         result.object.each do |follower_json|
           follower =  Githubber.new(follower_json)
           @followers << follower
@@ -58,11 +60,8 @@ class MainController < UIViewController
   end
 
   def get_following
-    puts 'GETTING FOLLOWING'
-    puts @user.following_url
     following_url = @user.following_url.split("{").first
     AFMotion::SessionClient.shared.get(following_url) do |result|
-      puts result.to_s
       if result.success?
         @following = []
 
@@ -77,6 +76,7 @@ class MainController < UIViewController
   end
 
   def sift_followers_from_following
+
     follower_ids = @followers.map(&:id)
     following_ids = @following.map(&:id)
 
@@ -84,11 +84,45 @@ class MainController < UIViewController
     following_names = []
     @following.each do |follower|
       if following_ids.include?(follower.id)
-        following_names << follower.login
+        @followers_not_following << follower
       end
-      non_followers_label.text = following_names.join(" ")
-      non_followers_label.sizeToFit
     end
+    followers_not_following_table.reloadData
   end
+
+  def followers_not_following_table
+    @followers_not_following_table ||= UITableView.alloc.initWithFrame([[0.0, 300.0], [320.0, 240.0]])
+  end
+
+  def add_followers_not_following_table
+    followers_not_following_table.backgroundView = nil
+    followers_not_following_table.backgroundColor = UIColor.turquoiseColor
+    followers_not_following_table.dataSource = self
+    followers_not_following_table.delegate = self
+    self.view.addSubview(followers_not_following_table)
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
+
+    unless cell
+      cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+      cell.selectionStyle = UITableViewCellSelectionStyleNone
+    end
+
+    follower = @followers_not_following[indexPath.row]
+    if follower
+      cell.textLabel.text = follower.login.to_s
+    end
+
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @followers_not_following.count || 0
+  end
+
 
 end
